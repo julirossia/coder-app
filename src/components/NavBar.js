05ce854d06ccd4362c308e68cd0716bd82278868
julirossia@gmail.com <julirossia@gmail.com>
2021-09-06 17:24:15 -0300

    -entrega Menú E-commerce.

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const Nav = () =>{
+    return(
+        <nav className="navbar">
+            <p>app</p>
+            <div className="links">
+            <a href="#">Inicio</a>
+            <a href="#">Productos</a>
+            <a href="#">Tienda</a>
+
+            </div>
+    </nav>
+    )
+}
+
+export default Nav;
